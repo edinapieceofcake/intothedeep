2d9873277cd1c36430f5e9510f20caa11659205a
clarkmilbert <clarkmilbert@gmail.com>
2024-10-15 18:44:03 -0500

    wait command instead of sleep for roadrunner paths

--- TeamCode/src/main/java/edu/edina/OpModes/Autonomous/MeepMeepRun.java
+++ TeamCode/src/main/java/edu/edina/OpModes/Autonomous/MeepMeepRun.java
@@ -25,54 +25,45 @@ public class MeepMeepRun extends LinearOpMode  {
 
             waitForStart();
 
-            sleep(1000);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(-48, -38), 1.0 / 2 * Math.PI).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(-60, -50), 1.0 / 2 * Math.PI).build());
-            sleep(1000);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(-58, -38), 1.0 / 2 * Math.PI).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(-60, -50), 1.0 / 2 * Math.PI).build());
-            sleep(1000);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(-57, -25), Math.PI).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(-60, -50), 1.0 / 2 * Math.PI).build());
-            for (int i = 0; i < 3; i++)
-            {
-                sleep(1000);
-                Actions.runBlocking(drive.actionBuilder(beginPose).setReversed(false).splineTo(new Vector2d(-22, 0), 0).build());
-                sleep(1000);
-                Actions.runBlocking(drive.actionBuilder(beginPose).setReversed(true).splineTo(new Vector2d(-60, -50), 3.0 / 2 * Math.PI).build());
-            }
-            // WITHOUT SLEEPS
-            /*
             Actions.runBlocking(
                     drive.actionBuilder(beginPose)
 
                             // NEUTRAL SAMPLES
                             // PASTE HERE
+                            .waitSeconds(1)
                             .strafeToLinearHeading(new Vector2d(-48, -38), 1.0 / 2 * Math.PI)
+                            .waitSeconds(0.5)
                             .strafeToLinearHeading(new Vector2d(-60, -50), 1.0 / 2 * Math.PI)
+                            .waitSeconds(1)
                             .strafeToLinearHeading(new Vector2d(-58, -38), 1.0 / 2 * Math.PI)
+                            .waitSeconds(0.5)
                             .strafeToLinearHeading(new Vector2d(-60, -50), 1.0 / 2 * Math.PI)
+                            .waitSeconds(1)
                             .strafeToLinearHeading(new Vector2d(-57, -25), Math.PI)
+                            .waitSeconds(0.5)
                             .strafeToLinearHeading(new Vector2d(-60, -50), 1.0 / 2 * Math.PI)
+                            .waitSeconds(1)
                             .setReversed(false)
                             .splineTo(new Vector2d(-22, 0), 0)
+                            .waitSeconds(1)
                             .setReversed(true)
                             .splineTo(new Vector2d(-60, -50), 3.0 / 2 * Math.PI)
+                            .waitSeconds(1)
                             .setReversed(false)
                             .splineTo(new Vector2d(-22, 0), 0)
+                            .waitSeconds(1)
                             .setReversed(true)
                             .splineTo(new Vector2d(-60, -50), 3.0 / 2 * Math.PI)
+                            .waitSeconds(1)
                             .setReversed(false)
                             .splineTo(new Vector2d(-22, 0), 0)
+                            .waitSeconds(1)
                             .setReversed(true)
                             .splineTo(new Vector2d(-60, -50), 3.0 / 2 * Math.PI)
                             // STOP PASTING
 
                             .build());
-             */
+
         }
         else {
             // *****SPECIMENS*****
@@ -85,69 +76,50 @@ public class MeepMeepRun extends LinearOpMode  {
 
             waitForStart();
 
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeTo(new Vector2d(0,-35)).build());
-            sleep(1000);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeTo(new Vector2d(47,-60)).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeTo(new Vector2d(0,-35)).build());
-            sleep(1000);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeTo(new Vector2d(47,-60)).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeTo(new Vector2d(0,-35)).build());
-            sleep(1000);
-            Actions.runBlocking(drive.actionBuilder(beginPose).setReversed(false).splineTo(new Vector2d(38,-25), 0).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(47,-60), 3.0/2*Math.PI).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeTo(new Vector2d(0,-35)).build());
-            sleep(1000);
-            Actions.runBlocking(drive.actionBuilder(beginPose).setReversed(false).splineTo(new Vector2d(48,-25), 0).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(47,-60), 3.0/2*Math.PI).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeTo(new Vector2d(0,-35)).build());
-            sleep(1000);
-            Actions.runBlocking(drive.actionBuilder(beginPose).setReversed(false).splineTo(new Vector2d(58,-25), 0).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(47,-60), 3.0/2*Math.PI).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeTo(new Vector2d(0,-35)).build());
-            sleep(1000);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeToLinearHeading(new Vector2d(47,-60), 3.0/2*Math.PI).build());
-            sleep(500);
-            Actions.runBlocking(drive.actionBuilder(beginPose).strafeTo(new Vector2d(0,-35)).build());
-            sleep(1000);
-
-            // WITHOUT DELAYS
-            /*
             Actions.runBlocking(
                     drive.actionBuilder(beginPose)
 
                             // NEUTRAL SAMPLES
                             // PASTE HERE
                             .strafeTo(new Vector2d(0,-35))
+                            .waitSeconds(1)
                             .strafeTo(new Vector2d(47,-60))
+                            .waitSeconds(0.5)
                             .strafeTo(new Vector2d(0,-35))
+                            .waitSeconds(1)
                             .strafeTo(new Vector2d(47,-60))
+                            .waitSeconds(0.5)
                             .strafeTo(new Vector2d(0,-35))
+                            .waitSeconds(1)
                             .setReversed(false)
                             .splineTo(new Vector2d(38,-25), 0)
+                            .waitSeconds(0.5)
                             .strafeToLinearHeading(new Vector2d(47,-60), 3.0/2*Math.PI)
+                            .waitSeconds(0.5)
                             .strafeTo(new Vector2d(0,-35))
+                            .waitSeconds(1)
                             .setReversed(false)
                             .splineTo(new Vector2d(48,-25), 0)
+                            .waitSeconds(0.5)
                             .strafeToLinearHeading(new Vector2d(47,-60), 3.0/2*Math.PI)
+                            .waitSeconds(0.5)
                             .strafeTo(new Vector2d(0,-35))
+                            .waitSeconds(1)
                             .setReversed(false)
                             .splineTo(new Vector2d(58,-25), 0)
+                            .waitSeconds(0.5)
                             .strafeToLinearHeading(new Vector2d(47,-60), 3.0/2*Math.PI)
+                            .waitSeconds(0.5)
                             .strafeTo(new Vector2d(0,-35))
+                            .waitSeconds(1)
                             .strafeToLinearHeading(new Vector2d(47,-60), 3.0/2*Math.PI)
+                            .waitSeconds(0.5)
                             .strafeTo(new Vector2d(0,-35))
+                            .waitSeconds(1)
                             // STOP PASTING
 
                             .build());
-             */
+
         }
     }
 }
