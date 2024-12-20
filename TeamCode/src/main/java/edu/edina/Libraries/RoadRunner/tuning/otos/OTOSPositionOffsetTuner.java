b2840621952e65ab5829bb275c2e8e71f64dd5cd
Avner Hegerle <avner.hegerle@gmail.com>
2024-12-20 08:48:04 -0600

    fix package names

--- TeamCode/src/main/java/edu/edina/Libraries/RoadRunner/tuning/otos/OTOSPositionOffsetTuner.java
+++ TeamCode/src/main/java/edu/edina/Libraries/RoadRunner/tuning/otos/OTOSPositionOffsetTuner.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.tuning.otos;
+package edu.edina.Libraries.RoadRunner.tuning.otos;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
