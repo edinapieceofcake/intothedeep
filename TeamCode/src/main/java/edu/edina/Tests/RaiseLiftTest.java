22d4bd81ab6c08363b729424896c2a4a69fa8d55
AlexanderBecker13 <144349466+AlexanderBecker13@users.noreply.github.com>
2025-01-07 20:08:00 -0600

    Renamed things and did kinematics test

---
+++ TeamCode/src/main/java/edu/edina/Tests/RaiseLiftTest.java
@@ -0,0 +1,36 @@
+package edu.edina.Tests;
+
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.R;
+
+import edu.edina.Libraries.Actions.RaiseLift;
+import edu.edina.Libraries.Robot.RobotHardware;
+@TeleOp
+
+public class RaiseLiftTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotHardware robotHardware = new RobotHardware(this);
+        waitForStart();
+        Action action;
+        while (opModeIsActive()) {
+            if (gamepad1.a){
+               action = new SequentialAction(
+                        new RaiseLift(robotHardware,14)
+                );
+
+            } else {
+                action = new SequentialAction(
+                        new RaiseLift(robotHardware,0)
+                );
+            }
+            action.run(new TelemetryPacket());
+        }
+    }
+}
