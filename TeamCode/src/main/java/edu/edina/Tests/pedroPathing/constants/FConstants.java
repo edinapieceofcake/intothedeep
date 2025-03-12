77656fe7d874434363712968e9904b0993406378
AlexanderBecker13 <144349466+AlexanderBecker13@users.noreply.github.com>
2025-03-11 19:12:16 -0500

    Added motor names and mass(KG)

--- TeamCode/src/main/java/edu/edina/Tests/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/edu/edina/Tests/pedroPathing/constants/FConstants.java
@@ -10,17 +10,17 @@ public class FConstants {
     static {
         FollowerConstants.localizers = Localizers.THREE_WHEEL;
 
-        FollowerConstants.leftFrontMotorName = "leftFront";
-        FollowerConstants.leftRearMotorName = "leftRear";
-        FollowerConstants.rightFrontMotorName = "rightFront";
-        FollowerConstants.rightRearMotorName = "rightRear";
+        FollowerConstants.leftFrontMotorName = "left_front_drive";
+        FollowerConstants.leftRearMotorName = "left_back_drive";
+        FollowerConstants.rightFrontMotorName = "right_front_drive";
+        FollowerConstants.rightRearMotorName = "right_back_drive";
 
         FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
         FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
         FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
         FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
 
-        FollowerConstants.mass = 13;
+        FollowerConstants.mass = 10.387;
 
         FollowerConstants.xMovement = 57.8741;
         FollowerConstants.yMovement = 52.295;
