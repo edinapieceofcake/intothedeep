252d8e8d95dfe766f7a1397b8c738325cf6a1e3e
Avner Hegerle <avner.hegerle@gmail.com>
2025-03-16 18:55:58 -0500

    disable tracking in sharp
    helps with oscillation

--- TeamCode/src/main/java/edu/edina/Tests/FieldCentricTest.java
+++ TeamCode/src/main/java/edu/edina/Tests/FieldCentricTest.java
@@ -14,6 +14,7 @@ import edu.edina.Libraries.Robot.OpticalOdometry;
 @TeleOp
 public class FieldCentricTest extends LinearOpMode {
     private Drivetrain dt;
+    public static double VEC_TRACK_ANGLE = 30;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -27,27 +28,33 @@ public class FieldCentricTest extends LinearOpMode {
 
         while (opModeIsActive()) {
             Pose2dDual<Time> poseDual = odo.getCurrentPoseDual();
+
             currPose = poseDual.value();
 
             Vector2d vel = poseDual.position.drop(1).value();
 
             Vector2d c = new Vector2d(-gamepad1.left_stick_y, -gamepad1.left_stick_x);
-            Vector2d p1 = currPose.position;
-            Vector2d q0 = refPose.position;
-            Vector2d q1 = project(p1.minus(q0), c).plus(q0);
 
-            refPose = new Pose2d(q1, refPose.heading);
+            if (angleBetweenDeg(c, vel) < VEC_TRACK_ANGLE) {
+                Vector2d p1 = currPose.position;
+                Vector2d q0 = refPose.position;
+                Vector2d q1 = project(p1.minus(q0), c).plus(q0);
 
-            Vector2d pursuitPoint = q1.plus(c);
+                refPose = new Pose2d(q1, refPose.heading);
+            } else {
+                refPose = currPose;
+            }
+
+            Vector2d pursuitPoint = refPose.position.plus(c);
 
             Vector2d relPursuitPoint = FieldToRobot.toRobotRel(currPose, pursuitPoint);
 
-            dt.update(.4 * relPursuitPoint.x, .4 * -relPursuitPoint.y, gamepad1.right_stick_x);
+            dt.update(relPursuitPoint.x, -relPursuitPoint.y, gamepad1.right_stick_x);
 
             telemetry.addData("c", "(%.1f, %.1f)", c.x, c.y);
-            telemetry.addData("pose", "(%.1f, %.1f) %f", p1.x, p1.y,
+            telemetry.addData("pose", "(%.1f, %.1f) %f", currPose.position.x, currPose.position.y,
                     Math.toDegrees(currPose.heading.toDouble()));
-            telemetry.addData("refPose", "(%.1f, %.1f) %f", q1.x, q1.y,
+            telemetry.addData("refPose", "(%.1f, %.1f) %f", refPose.position.x, refPose.position.y,
                     Math.toDegrees(refPose.heading.toDouble()));
             telemetry.addData("pursuit", "(%.1f, %.1f)", pursuitPoint.x, pursuitPoint.y);
             telemetry.addData("rel pursuit", "(%.1f, %.1f)", relPursuitPoint.x, relPursuitPoint.y);
@@ -61,5 +68,14 @@ public class FieldCentricTest extends LinearOpMode {
             return v;
         else
             return onto.times(v.dot(onto) / o2);
+    }
+
+    public Vector2d normalize(Vector2d v) {
+        return v.div(v.norm());
+    }
+
+    public double angleBetweenDeg(Vector2d v0, Vector2d v1) {
+        double rad = Math.acos(normalize(v0).dot(normalize(v1)));
+        return Math.toDegrees(rad);
     }
 }
