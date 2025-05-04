ebd6c05911241661423a115386cae28e9223515a
Avner Hegerle <avner.hegerle@gmail.com>
2025-05-03 21:05:12 -0500

    test current monitor sensor

--- TeamCode/src/main/java/edu/edina/Tests/CurrentMonitorTest.java
+++ TeamCode/src/main/java/edu/edina/Tests/CurrentMonitorTest.java
@@ -2,25 +2,69 @@ package edu.edina.Tests;
 
 import android.annotation.SuppressLint;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.nio.ByteBuffer;
 
 import edu.edina.Libraries.Robot.INA219Device;
 
 @TeleOp
 public class CurrentMonitorTest extends LinearOpMode {
+    private FtcDashboard dashboard;
+    private INA219Device dev;
+
     @SuppressLint("DefaultLocale")
     @Override
     public void runOpMode() throws InterruptedException {
-        INA219Device dev = hardwareMap.get(INA219Device.class, "sensor_ina219");
-        waitForStart();
+        DataFile dataFile = new DataFile("current-monitor.csv");
+        dataFile.println("Time,Reading");
+
+        dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+        dev = hardwareMap.get(INA219Device.class, "sensor_ina219");
+        DcMotor motor = hardwareMap.get(DcMotor.class, "arm_motor");
+
+        while (opModeInInit()) {
+            readDevice();
+            dataFile.showStatus(telemetry);
+            telemetry.update();
+        }
+
+        ElapsedTime t = new ElapsedTime();
+
         while (opModeIsActive()) {
-            byte[] buffer = dev.getReading();
-            for (int i = 0; i < buffer.length; i++) {
-                telemetry.addData(String.format("b%d", i), "%x", buffer[i]);
+            telemetry.addLine("press A to run motor");
+
+            if (gamepad1.a) {
+                motor.setPower(0.2);
+            } else {
+                motor.setPower(0);
             }
+
+            short reading = readDevice();
+            dataFile.println(String.format("%f,%d", t.seconds(), reading));
 
             telemetry.update();
         }
+    }
+
+    private short readDevice() {
+        byte[] buffer = dev.getReading();
+        short reading = ByteBuffer.wrap(buffer).getShort();
+
+        telemetry.addData("reading", reading);
+
+        TelemetryPacket p = new TelemetryPacket();
+        p.put("reading", reading);
+        dashboard.sendTelemetryPacket(p);
+
+        return reading;
     }
 }
---
+++ TeamCode/src/main/java/edu/edina/Tests/DataFile.java
@@ -0,0 +1,41 @@
+package edu.edina.Tests;
+
+import android.os.Environment;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class DataFile {
+    private File file;
+    private PrintStream out;
+
+    public DataFile(String name) {
+        File root = Environment.getExternalStorageDirectory();
+        file = new File(root, name);
+
+        try {
+            FileOutputStream stream = new FileOutputStream(file);
+            out = new PrintStream(stream);
+        } catch (Exception x) {
+            out = null;
+        }
+    }
+
+    public void showStatus(Telemetry telemetry) {
+        if (out != null) {
+            telemetry.addData("data file", file);
+        } else {
+            telemetry.addLine("could not open data file");
+        }
+    }
+
+    public void println(String s) {
+        if (out != null) {
+            out.print(s);
+            out.flush();
+        }
+    }
+}
