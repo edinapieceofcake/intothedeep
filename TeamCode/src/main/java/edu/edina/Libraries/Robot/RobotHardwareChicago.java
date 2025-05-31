6a5ff15e3b88718f11ad8e89a42b09520280d39e
Avner Hegerle <avner.hegerle@gmail.com>
2025-05-31 11:10:56 -0500

    document hardware map

--- TeamCode/src/main/java/edu/edina/Libraries/Robot/RobotHardwareChicago.java
+++ TeamCode/src/main/java/edu/edina/Libraries/Robot/RobotHardwareChicago.java
@@ -10,6 +10,36 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Control hub:
+ *   Motor 0: extension_motor
+ *   Motor 1: left_front_drive
+ *   Motor 2: left_back_drive
+ *   Motor 3: left_lift_motor
+ *   Servo 3: swivel
+ *   Servo 4: wrist
+ *   Servo 5: claw_top
+ *   I2C Bus 0:
+ *     imu
+ *     sensor_ina260
+ *   I2C Bus 1:
+ *     neopixel_driver
+ *   I2C Bus 2:
+ *     sensor_otos
+ *   I2C Bus 3:
+ *     sensor_color
+ * Expansion hub:
+ *   Motor 0: right_lift_motor
+ *   Motor 1: right_back_drive
+ *   Motor 2: right_front_drive
+ *   Motor 3: arm_motor
+ *   Servo 3: claw_bottom
+ *   I2C 1:
+ *     distance_left
+ *   I2C 2:
+ *     distance_right
+ */
+
 public class RobotHardwareChicago {
     private FtcDashboard dash = FtcDashboard.getInstance();
     private List<Action> runningActions = new ArrayList<>();
@@ -20,6 +50,7 @@ public class RobotHardwareChicago {
     private RobotState robotState;
     private TelemetryPacket packet;
     private CurrentSensor currentSensor;
+    private Light light;
 
     public RobotHardwareChicago(HardwareMap hw) {
         packet = new TelemetryPacket();
