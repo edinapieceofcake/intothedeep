2e0357cea661b7c4ffe8ff6917870aeb6ac09262
Avner Hegerle <avner.hegerle@gmail.com>
2025-06-02 22:09:33 -0500

    initial servo actions (needs updating)

---
+++ TeamCode/src/main/java/edu/edina/Libraries/Robot/Grabber.java
@@ -0,0 +1,68 @@
+package edu.edina.Libraries.Robot;
+
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.acmerobotics.roadrunner.Action;
+
+public class Grabber {
+    private Servo claw, wrist, swivel;
+    private RobotState robotState;
+
+    //claw
+    public static double OPEN_POS = 0.576;
+    public static double CLOSED_POS = 0.53;
+
+    //wrist
+    public static double INTAKE_POSITION = 0.4;
+    public static double SPECIMEN_POSITION = 1;
+    public static double WALL_POSITION = (INTAKE_POSITION + SPECIMEN_POSITION) / 2;
+
+    //swivel
+    public static double HORIZONTAL_POS = 0;
+    public static double VERTICAL_POS = 0.576;
+
+    private boolean open;
+
+    public Grabber(RobotState robotState, HardwareMap hw) {
+        claw = hw.get(Servo.class, "claw_bottom");
+        wrist = hw.get(Servo.class, "wrist");
+        swivel = hw.get(Servo.class, "swivel");
+
+        open = false;
+
+        this.robotState = robotState;
+    }
+
+    //update with tracking and cancelable etc.
+    public Action toggleClaw() {
+        double pos = open ? CLOSED_POS : OPEN_POS;
+        open = !open;
+        return new InstantAction(() -> claw.setPosition(pos));
+    }
+
+    public Action subMode() {
+        return new InstantAction(() -> {
+            wrist.setPosition(INTAKE_POSITION);
+            claw.setPosition(OPEN_POS);
+            swivel.setPosition(HORIZONTAL_POS);
+        });
+    }
+
+    public Action specimenMode() {
+        return new InstantAction(() -> {
+            wrist.setPosition(SPECIMEN_POSITION);
+            claw.setPosition(CLOSED_POS);
+            swivel.setPosition(HORIZONTAL_POS);
+        });
+    }
+
+    public Action wallMode() {
+        return new InstantAction(() -> {
+            wrist.setPosition(WALL_POSITION);
+            claw.setPosition(OPEN_POS);
+            swivel.setPosition(HORIZONTAL_POS);
+        });
+    }
+}
