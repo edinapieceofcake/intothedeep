720b82d1f90a94ba19b23a64b2673e0f1077e132
Avner Hegerle <avner.hegerle@gmail.com>
2025-07-13 09:56:45 -0500

    work on auto sample alliance

--- TeamCode/src/main/java/edu/edina/OpModes/Autonomous/AutoSampleAllianceSide.java
+++ TeamCode/src/main/java/edu/edina/OpModes/Autonomous/AutoSampleAllianceSide.java
@@ -18,14 +18,12 @@ import edu.edina.Libraries.Robot.WaitForTime;
 public class AutoSampleAllianceSide extends AutoBase {
     public static double START_X = -40, START_Y = 61, START_H = 0;
     public static double BASKET_X = -37, BASKET_Y = 87, BASKET_H = 135, BASKET_MAX_SPEED = 35, BASKET_RADIUS = 5;
-    public static double FIRST_SPIKE_X = -20, FIRST_SPIKE_Y = 76, FIRST_SPIKE_H = 180, FIRST_SPIKE_MAX_SPEED = 35, FIRST_SPIKE_RADIUS = 4;
-    public static double SECOND_SPIKE_X = -16, SECOND_SPIKE_Y = 81, SECOND_SPIKE_H = 0, SECOND_SPIKE_MAX_SPEED = 24, SECOND_SPIKE_RADIUS = 5;
-    public static double THIRD_SPIKE_X = -16, THIRD_SPIKE_Y = 88, THIRD_SPIKE_H = 0, THIRD_SPIKE_MAX_SPEED = 24, THIRD_SPIKE_RADIUS = 5;
-    public static double OPP_FIRST_SPIKE_X = 16, OPP_FIRST_SPIKE_Y = 73, OPP_FIRST_SPIKE_H = 0, OPP_FIRST_SPIKE_MAX_SPEED = 24, OPP_FIRST_SPIKE_RADIUS = 5;
-    public static double OPP_SECOND_SPIKE_X = 16, OPP_SECOND_SPIKE_Y = 81, OPP_SECOND_SPIKE_H = 0, OPP_SECOND_SPIKE_MAX_SPEED = 24, OPP_SECOND_SPIKE_RADIUS = 5;
-    public static double OPP_THIRD_SPIKE_X = 16, OPP_THIRD_SPIKE_Y = 88, OPP_THIRD_SPIKE_H = 0, OPP_THIRD_SPIKE_MAX_SPEED = 24, OPP_THIRD_SPIKE_RADIUS = 5;
-
-    private Path score, firstSpikeMark, secondSpikeMark, thirdSpikeMark, oppFirstSpikeMark, oppSecondSpikeMark, oppThirdSpikeMark;
+    public static double FIRST_SPIKE_X = -22, FIRST_SPIKE_Y = 76, FIRST_SPIKE_H = 180, FIRST_SPIKE_MAX_SPEED = 35, FIRST_SPIKE_RADIUS = 4;
+    public static double SECOND_SPIKE_X = -22, SECOND_SPIKE_Y = 85, SECOND_SPIKE_H = 0, SECOND_SPIKE_MAX_SPEED = 24, SECOND_SPIKE_RADIUS = 5;
+    public static double THIRD_SPIKE_X = -22, THIRD_SPIKE_Y = 88, THIRD_SPIKE_H = 0, THIRD_SPIKE_MAX_SPEED = 24, THIRD_SPIKE_RADIUS = 5;
+    public static double OPP_FIRST_SPIKE_X = 22, OPP_FIRST_SPIKE_Y = 76, OPP_FIRST_SPIKE_H = 0, OPP_FIRST_SPIKE_MAX_SPEED = 24, OPP_FIRST_SPIKE_RADIUS = 5;
+    public static double OPP_SECOND_SPIKE_X = 22, OPP_SECOND_SPIKE_Y = 85, OPP_SECOND_SPIKE_H = 0, OPP_SECOND_SPIKE_MAX_SPEED = 24, OPP_SECOND_SPIKE_RADIUS = 5;
+    public static double OPP_THIRD_SPIKE_X = 22, OPP_THIRD_SPIKE_Y = 88, OPP_THIRD_SPIKE_H = 0, OPP_THIRD_SPIKE_MAX_SPEED = 24, OPP_THIRD_SPIKE_RADIUS = 5;
 
     public AutoSampleAllianceSide() {
         super(START_X, START_Y, START_H);
@@ -33,52 +31,79 @@ public class AutoSampleAllianceSide extends AutoBase {
 
     @Override
     public void initAuto() {
-        score = new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(BASKET_X, BASKET_Y)})
-                .withMaxSpeed(BASKET_MAX_SPEED)
-                .withRadius(BASKET_RADIUS)
-                .withHeading(BASKET_H);
-        firstSpikeMark = new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(FIRST_SPIKE_X, FIRST_SPIKE_Y)})
-                .withMaxSpeed(FIRST_SPIKE_MAX_SPEED)
-                .withRadius(FIRST_SPIKE_RADIUS)
-                .withHeading(FIRST_SPIKE_H);
-        secondSpikeMark = new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(SECOND_SPIKE_X, SECOND_SPIKE_Y)})
-                .withMaxSpeed(SECOND_SPIKE_MAX_SPEED)
-                .withRadius(SECOND_SPIKE_RADIUS)
-                .withHeading(SECOND_SPIKE_H);
-        thirdSpikeMark = new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(THIRD_SPIKE_X, THIRD_SPIKE_Y)})
-                .withMaxSpeed(THIRD_SPIKE_MAX_SPEED)
-                .withRadius(THIRD_SPIKE_RADIUS)
-                .withHeading(THIRD_SPIKE_H);
-        oppFirstSpikeMark = new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(OPP_FIRST_SPIKE_X, OPP_FIRST_SPIKE_Y)})
-                .withMaxSpeed(OPP_FIRST_SPIKE_MAX_SPEED)
-                .withRadius(OPP_FIRST_SPIKE_RADIUS)
-                .withHeading(OPP_FIRST_SPIKE_H);
-        oppSecondSpikeMark = new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(OPP_SECOND_SPIKE_X, OPP_SECOND_SPIKE_Y)})
-                .withMaxSpeed(OPP_SECOND_SPIKE_MAX_SPEED)
-                .withRadius(OPP_SECOND_SPIKE_RADIUS)
-                .withHeading(OPP_SECOND_SPIKE_H);
-        oppThirdSpikeMark = new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(OPP_THIRD_SPIKE_X, OPP_THIRD_SPIKE_Y)})
-                .withMaxSpeed(OPP_THIRD_SPIKE_MAX_SPEED)
-                .withRadius(OPP_THIRD_SPIKE_RADIUS)
-                .withHeading(OPP_THIRD_SPIKE_H);
-
         hw.enableYellow();
 
         hw.addAction(
                 new SequentialAction(
                         new InstantAction(hw::wallMode),
-                        hw.sequencePath(score, 3),
+                        hw.sequencePath(score(), 3),
                         new InstantAction(hw::highBasketRear),
                         new WaitUntil(() -> state.getExtensionPos() > Extension.POS_HIGH_BASKET - 1),
                         new WaitForTime(200),
                         new InstantAction(() -> hw.openClaw()),
                         new WaitForTime(50),
                         new ParallelAction(
-                                hw.sequencePath(firstSpikeMark, 3),
+                                hw.sequencePath(firstSpikeMark(), 3),
                                 new InstantAction(hw::ground)
                         ),
-                        new SampleAlignAction(hw)
+                        new InstantAction(hw::groundIntake),
+                        new WaitForTime(100),
+                        hw.sequencePath(score(), 3),
+                        new WaitUntil(() -> state.getExtensionPos() > Extension.POS_HIGH_BASKET - 1),
+                        new WaitForTime(200),
+                        new InstantAction(() -> hw.openClaw()),
+                        new WaitForTime(50),
+
                 )
         );
+    }
+
+    public Path score() {
+        return new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(BASKET_X, BASKET_Y)})
+                .withMaxSpeed(BASKET_MAX_SPEED)
+                .withRadius(BASKET_RADIUS)
+                .withHeading(BASKET_H);
+    }
+
+    public Path firstSpikeMark() {
+        return new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(FIRST_SPIKE_X, FIRST_SPIKE_Y)})
+                .withMaxSpeed(FIRST_SPIKE_MAX_SPEED)
+                .withRadius(FIRST_SPIKE_RADIUS)
+                .withHeading(FIRST_SPIKE_H);
+    }
+
+    public Path secondSpikeMark() {
+        return new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(SECOND_SPIKE_X, SECOND_SPIKE_Y)})
+                .withMaxSpeed(SECOND_SPIKE_MAX_SPEED)
+                .withRadius(SECOND_SPIKE_RADIUS)
+                .withHeading(SECOND_SPIKE_H);
+    }
+
+    public Path thirdSpikeMark() {
+        return new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(THIRD_SPIKE_X, THIRD_SPIKE_Y)})
+                .withMaxSpeed(THIRD_SPIKE_MAX_SPEED)
+                .withRadius(THIRD_SPIKE_RADIUS)
+                .withHeading(THIRD_SPIKE_H);
+    }
+
+    public Path oppFirstSpikeMark() {
+        return new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(OPP_FIRST_SPIKE_X, OPP_FIRST_SPIKE_Y)})
+                .withMaxSpeed(OPP_FIRST_SPIKE_MAX_SPEED)
+                .withRadius(OPP_FIRST_SPIKE_RADIUS)
+                .withHeading(OPP_FIRST_SPIKE_H);
+    }
+
+    public Path oppSecondSpikeMark() {
+        return new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(OPP_SECOND_SPIKE_X, OPP_SECOND_SPIKE_Y)})
+                .withMaxSpeed(OPP_SECOND_SPIKE_MAX_SPEED)
+                .withRadius(OPP_SECOND_SPIKE_RADIUS)
+                .withHeading(OPP_SECOND_SPIKE_H);
+    }
+
+    public Path oppThirdSpikeMark() {
+        return new Path(new Vector2d[]{state.getCurrentPose().position, new Vector2d(OPP_THIRD_SPIKE_X, OPP_THIRD_SPIKE_Y)})
+                .withMaxSpeed(OPP_THIRD_SPIKE_MAX_SPEED)
+                .withRadius(OPP_THIRD_SPIKE_RADIUS)
+                .withHeading(OPP_THIRD_SPIKE_H);
     }
 }
